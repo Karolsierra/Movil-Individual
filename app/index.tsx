8410f62fb6357f0c063d922a7a1e2709fbe25f3c
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2024-08-06 10:58:11 -1200

    se encuentra todo hasta el 3 video

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import {StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Link} from "expo-router";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={style.container}>
+      <Text style={style.text}>Aora ADSO</Text>
+      <StatusBar style='auto' />
+      <Link  href='/profile' style={{color:'blue'}}>Go to Profile</Link>
     </View>
-  );
+  )
 }
+
+const style=StyleSheet.create({
+
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  text:{
+    fontSize:24,
+  },
+})
