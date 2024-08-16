ea946a8499ccbde93baee9f97cea2b06ca00fdcc
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2024-08-16 07:34:28 -1200

    nuevas carpetas y archivos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,30 @@
-import { Slot,Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { SplashScreen,Stack } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout=() =>{
+    const[fontsLoaded, error] = useFonts({
+      "Poppins-black": require ("../assets/fonts/Poppins-Black.ttf"),
+      "Poppins-bold": require ("../assets/fonts/Poppins-Bold.ttf"),
+      "Poppins-extraBold": require ("../assets/fonts/Poppins-ExtraBold.ttf"),
+      "Poppins-extraLight": require ("../assets/fonts/Poppins-ExtraLight.ttf"),
+      "Poppins-light": require ("../assets/fonts/Poppins-Light.ttf"),
+      "Poppins-medium": require ("../assets/fonts/Poppins-Medium.ttf"),
+      "Poppins-regular": require ("../assets/fonts/Poppins-Regular.ttf"),
+      "Poppins-semibold": require ("../assets/fonts/Poppins-Semibold.ttf"),
+      "Poppins-thin": require ("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  useEffect(() =>{
+    if(error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
+  },[fontsLoaded,error])
+
+  if(!fontsLoaded && !error) return null;
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{headerShown: false}}/>
--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,14 @@
-import {StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link} from "expo-router";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
   return (
-    <View style={style.container}>
-      <Text style={style.text}>Aora ADSO</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text clasName="text-3xl font-pblanck">Aora ADSO</Text>
       <StatusBar style='auto' />
-      <Link  href='/profile' style={{color:'blue'}}>Go to Profile</Link>
+      <Link  href='/home' style={{color:'blue'}}>Go to Home</Link>
     </View>
   )
 }
 
-const style=StyleSheet.create({
-
-  container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-  },
-  text:{
-    fontSize:24,
-  },
-})
